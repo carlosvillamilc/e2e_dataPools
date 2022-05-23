4d869752b49c02c36a1c6e89abff4f8bc2e383c5
Carlos Villamil <c.villamilc@uniandes.edu.co>
2022-05-22 23:59:31 -0500

    Ajuste casos designs_juan

--- cypress/integration/designs_Juan.js
+++ cypress/integration/designs_Juan.js
@@ -23,8 +23,8 @@ describe('Design Cases', () => {
         cy.get('#ember10').type(password)
         cy.get('#ember12').click()        
         cy.get('a[href="#/settings/design/"]').eq(0).click()
-        cy.get('input[placeholder="Label"]').type('')
-        cy.get('button[class="gh-blognav-add"]').click()
+    
+    //    cy.get('button[class="gh-blognav-add"]').click()
         cy.get('button[class="gh-btn gh-btn-blue gh-btn-icon ember-view"]').click()
 
         cy.get('div.gh-nav-bottom').click()
@@ -39,8 +39,8 @@ it('Create a navigation label with numbers in  input', () => {
         cy.get('#ember10').type(password)
         cy.get('#ember12').click()        
         cy.get('a[href="#/settings/design/"]').eq(0).click()
-        cy.get('input[placeholder="Label"]').type(faker.datatype.number({ max: 99 }))
-        cy.get('button[class="gh-blognav-add"]').click()
+        cy.xpath('/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/section[1]/div[2]/form[1]/div[2]/div[1]/span[1]/input[1]').type(faker.datatype.number({ max: 99 }))
+//        cy.xpath('/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/section[1]/div[2]/form[1]/div[2]/button[1]/*[name()='svg'][1]').click()
         cy.get('button[class="gh-btn gh-btn-blue gh-btn-icon ember-view"]').click()
 
         cy.get('div.gh-nav-bottom').click()
@@ -54,8 +54,8 @@ it('Create a navigation label with empty URL', () => {
         cy.get('#ember10').type(password)
         cy.get('#ember12').click()        
         cy.get('a[href="#/settings/design/"]').eq(0).click()
-        cy.get('input[placeholder="Label"]').type(faker.datatype.number({ max: 99 }))
-        cy.get('button[class="gh-blognav-add"]').click()
+        cy.xpath('/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/section[1]/div[2]/form[1]/div[2]/div[1]/span[1]/input[1]').type(faker.datatype.number({ max: 99 }))
+//        cy.get('button[class="gh-blognav-add"]').click()
         cy.get('button[class="gh-btn gh-btn-blue gh-btn-icon ember-view"]').click()
 
         cy.get('div.gh-nav-bottom').click()
